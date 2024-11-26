a4fb95b2d9ee61f7905cf41f16d8b4056e2f43c4
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2024-11-26 17:33:53 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ EOF
 build_args=(
   "-Csetup-args=--warnlevel=0"
   "-Csetup-args=--cross-file=${SRC_DIR}/pkgconfig.ini"
+  "-Csetup-args=-Dpython_version=${PREFIX}/bin/python3"
 )
 if [ -f ${BUILD_PREFIX}/meson_cross_file.txt ]; then
   build_args=("${build_args[@]}" "-Csetup-args=--cross-file=${BUILD_PREFIX}/meson_cross_file.txt")
